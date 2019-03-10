725c4150270c2c975ba0c29a22063678029a6852
Jackie Tieu <jackietieu2@gmail.com>
2019-03-09 17:58:12 -0800

    updated React scripts

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const app = express();
 
-// helper function for date checking
-// const dateConverter = date => {
-//   let dateArray = date.split('-');
-
-//   return new Date(+dateArray[0], +dateArray[1] - 1, +dateArray[2]);
-// }
-
 app.get('/api/budgetCalculator', (req, res) => {
   // req.query = {startDate, numberOfDays}
   // query is already validated on the frontend
@@ -56,60 +49,9 @@ app.get('/api/budgetCalculator', (req, res) => {
   }
 })
 
-// api endpoint nested under /api dir
-// app.get('/api/budgetCalculator', (req, res) => {
-//   // req.query = {startDate, numberOfDays}
-//   // query is already validated on the frontend
-//   if (req.query.startDate && req.query.numberOfDays) {
-//     let totalCost = 0;
-//     let duration = req.query.numberOfDays;
-//     let date = dateConverter(req.query.startDate);
-
-//     while (duration > 0) {
-//       let currentDay = date.getDay(); //0 or 6 is a weekend, 1-6 weekday
-//       let currentDate = date.getDate(); //1-31
-//       let currentMonth = date.getMonth(); //0-11
-//       let currentYear = date.getFullYear(); //actual year
-
-//       if (currentDay !== 0 && currentDay !== 6) {
-//         let currentCost = 0;
-
-//         if (currentDate > 28) {
-//           currentCost = 0.25;
-//         } else if (currentDate > 21) {
-//           currentCost = 0.2;
-//         } else if (currentDate > 14) {
-//           currentCost = 0.15;
-//         } else if (currentDate > 7) {
-//           currentCost = 0.1;
-//         } else if (currentDate > 0) {
-//           currentCost = 0.05;
-//         }
-
-//         totalCost += currentCost;
-//       }
-      
-//       date = new Date(currentYear, currentMonth, currentDate + 1)
-//       duration--;
-//     }
-
-//     let startDate = dateConverter(req.query.startDate);
-
-//     res.status(200).json({
-//       totalCost: totalCost.toFixed(2),
-//       startDate: `${startDate.getMonth() + 1}-${startDate.getDate()}-${startDate.getFullYear()}`,
-//       numberOfDays: req.query.numberOfDays
-//     })
-//   } else {
-//     res.status(404).json({
-//       message: 'Unable to calculate'
-//     })
-//   }
-// })
-
 // default to React app for all other requests
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 })
 
 // if no routes can handle the req, it goes to this
